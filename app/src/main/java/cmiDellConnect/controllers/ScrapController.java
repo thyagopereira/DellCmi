7df3140010b189521be8ca680ed422d1baf679a0
thyagopereira <thyago.pereira.silva@ccc.ufcg.edu.br>
2022-06-29 20:45:06 -0300

    Connection Try Asyncronous Trouble

--- app/src/main/java/cmiDellConnect/controllers/ScrapController.java
+++ app/src/main/java/cmiDellConnect/controllers/ScrapController.java
@@ -1,10 +1,15 @@
 package cmiDellConnect.controllers;
 
 
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import cmiDellConnect.services.ScrapService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 
@@ -13,13 +18,28 @@ import io.swagger.annotations.ApiOperation;
 @Api(value= "Healthcheck")
 public class ScrapController {
 
-    private String URL = "";
+    private String HOST = "http://owc-lgd.dsc.ufcg.edu.br";
+    private String PORT = "8081" ;
+
+    @Autowired
+    private ScrapService service  = new ScrapService();
 
     @ApiOperation(value = "HealthCheck")
     @GetMapping("/health")
     public ResponseEntity<String> getHealth() {
         return new ResponseEntity<String>("Service is running on door :8080. With safe Connection.", HttpStatus.OK);
-    }
-    
+    } 
 
+    @ApiOperation(value = "Init a connection with the specified URL")
+    @GetMapping("/init")
+    public ResponseEntity<String> getResponse(){
+        try {
+            return new ResponseEntity<String>(service.initConnection(HOST, PORT), HttpStatus.OK);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return new ResponseEntity<String>("Proxy is not right setted" , HttpStatus.BAD_GATEWAY);
+
+    }
 }
---
+++ app/src/main/java/cmiDellConnect/services/ScrapService.java
@@ -0,0 +1,53 @@
+package cmiDellConnect.services;
+
+import java.net.Authenticator;
+import java.net.InetSocketAddress;
+import java.net.ProxySelector;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpClient.Redirect;
+import java.net.http.HttpClient.Version;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.time.Duration;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ScrapService {
+
+    public String initConnection(String host, String port) {
+        HttpClient client = getClient(host, Integer.parseInt(port));
+
+        HttpResponse<String> reponse = client.send(createRequest(), BodyHandlers.ofString());
+
+        return reponse;
+    }
+
+
+    private HttpClient getClient(String host, int port){
+        HttpClient client = HttpClient.newBuilder()
+            .version(Version.HTTP_1_1)
+            .followRedirects(Redirect.NORMAL)
+            .connectTimeout(Duration.ofSeconds(60))
+            .proxy(ProxySelector.of(new InetSocketAddress(host, port)))
+            .authenticator(Authenticator.getDefault())
+            .build();
+        
+        
+        return client ;            
+    }
+
+
+    private HttpRequest createRequest(){
+        return HttpRequest.newBuilder()
+        .uri(URI.create("http://owc-lqd.dsc.ufcg.edu.br:8081/cs/"))
+        .timeout(Duration.ofMinutes(2))
+        .header("Content-Type", "application/json")
+        .GET()
+        .build();
+    }
+
+    
+}
